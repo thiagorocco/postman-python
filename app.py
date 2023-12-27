e04cf632f0bd3b4c7a8274c3a9dd14258965e96c
thiagorocco <deroccotr@gmail.com>
2023-12-27 07:12:55 -0300

    Adicionado o arquivo app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+# API - É um lugar para disponibilizar recursos e/ou funcionalidades
+# 1. Objetivo: Criar um API que disponibiliza a consulta, criação, edição e exclusão de livros
+# 2. URL Base - localhost
+# 3. Endpoints
+# 4. Quais recursos
