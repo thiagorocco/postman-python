7f8c1dea59ace34c5d2f2eb11b4fad4a3e56dac5
thiagorocco <deroccotr@gmail.com>
2023-12-29 06:25:17 -0300

    Reestruturação de pastas, uma para flask e outra para django

--- app.py
+++
@@ -1,5 +0,0 @@
-# API - É um lugar para disponibilizar recursos e/ou funcionalidades
-# 1. Objetivo: Criar um API que disponibiliza a consulta, criação, edição e exclusão de livros
-# 2. URL Base - localhost
-# 3. Endpoints
-# 4. Quais recursos
