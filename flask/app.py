3c32e78b3f8e8cc3e711acc3650f4276862a64e9
thiagorocco <deroccotr@gmail.com>
2023-12-31 12:25:43 -0300

    função incluir_novo_livro criada

--- flask/app.py
+++ flask/app.py
@@ -41,6 +41,13 @@ def editar_livro_por_id (id):
             livros[indice].update(livro_alterado)
             return jsonify(livros[indice])
 
+#criar
+@app.route('/livros',methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(livros)
+
 # Excluir
 
 app.run(port=5000, host='localhost',debug=True)
