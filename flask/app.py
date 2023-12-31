24de54e34632e7861db6c7ae690c33ce7ea573f7
thiagorocco <deroccotr@gmail.com>
2023-12-31 11:14:03 -0300

    Criado os métodos obter_livro e obter_livro_por_id

--- flask/app.py
+++ flask/app.py
@@ -21,11 +21,26 @@ livros = [
 ]
 
 # Consultar (todos)
-@app.route('/livros')
+@app.route('/livros',methods=['GET'])
 def obterLivros():
     return jsonify(livros)
+
 # Consultar (id)
+@app.route('/livros/<int:id>',methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
 # Editar
+@app.route('/livros/<int:id>',methods=['PUT'])
+def editar_livro_por_id (id):
+    livro_alterado = request.get_json()
+    for indice, livro, in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
 # Excluir
 
 app.run(port=5000, host='localhost',debug=True)
